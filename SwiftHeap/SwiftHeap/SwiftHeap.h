6ee4741ca657865e37a936c3c8fef509e3ff2fdd
Olexa Boyko <boyko.olexa@gmail.com>
2017-02-17 11:49:26 +0200

    Initial commit

---
+++ SwiftHeap/SwiftHeap/SwiftHeap.h
@@ -0,0 +1,19 @@
+//
+//  SwiftHeap.h
+//  SwiftHeap
+//
+//  Created by Leobit on 2/17/17.
+//  Copyright © 2017 Lviv National University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftHeap.
+FOUNDATION_EXPORT double SwiftHeapVersionNumber;
+
+//! Project version string for SwiftHeap.
+FOUNDATION_EXPORT const unsigned char SwiftHeapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftHeap/PublicHeader.h>
+
+
